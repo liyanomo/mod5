e264c87d501def1cc2a24078882d670990d4dc44
Olga <olyusik.soloveva@mail.ru>
2023-03-06 21:45:11 +0300

    3

--- 52.py
+++ 52.py
@@ -4,7 +4,11 @@ list1 = {}
 with open ('l.json', 'w') as file:
     json.dump(list1,file)
     
-class model:
+class Model:
+
+    title = 1
+    text = 2
+    author = 3
     
     def _init_( self, title, text, author):
         self.title = title 
@@ -13,22 +17,16 @@ class model:
         self.list1 = list1
         self.slv = slv
 
- 
-    def save(self):
-            slv = {}
-            slv.append (text)
-            slv.append(author)
-            list1[title] = slv
-            l1 = json.dump(list1._dict_)
-            return (l1)
+    @staticmethod
+    
+    def save1 (self):
+        self.slv = {}
+        self.slv.append (self.text)
+        self.slv.append(self.author)
+        self.list1[title] = self.slv
+        self.l = json.dump(list1._dict_)
+        return (self.l)
             
-        
-while True:
-    avt = input ('ввести текст ?')
-    if avt == 'да':
-            title = input ("введите заголовок:")
-            text = input ("введите текст:")
-            author = input ("введите автора:")
-            print (save(self))
-    else:
-        break
+s = Model()
+s.save1 (self.l)
+print (s.save1)
